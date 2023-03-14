88ea05b436e77e9c84651426f0e205e8f98ee8f2
kubra-acar <acarkubra1.kba@gmail.com>
2023-03-14 13:15:18 +0300

    Proje dosyası ekle.

---
+++ rastgele5inkatlari/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace rastgele5inkatlari
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Gainsboro;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(267, 78);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(134, 308);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.BackColor = System.Drawing.Color.Gainsboro;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(519, 74);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(134, 308);
+            this.listBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Gainsboro;
+            this.textBox1.Location = new System.Drawing.Point(88, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Gainsboro;
+            this.textBox2.Location = new System.Drawing.Point(88, 141);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(117, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Plum;
+            this.button1.Location = new System.Drawing.Point(114, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 56);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Rastgele sayı üret";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Plum;
+            this.button2.Location = new System.Drawing.Point(407, 211);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 56);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "5\'e bölünenleri bul";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(13, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "1.sayı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(13, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "2.sayı";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(264, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 51);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "1. sayı ve 2.sayı arasından üretilen rastgele sayılar";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(516, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 51);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Rastgele sayılardan 5\'in katları olan sayılar";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(159, 287);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Temizle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.ClientSize = new System.Drawing.Size(711, 407);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ rastgele5inkatlari/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rastgele5inkatlari
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] sayilar = new int[10];
+            int sayi1 = 0, sayi2 = 0;
+            sayi1 = Convert.ToInt32(textBox1.Text);
+            sayi2 = Convert.ToInt32(textBox2.Text);
+            Random rnd = new Random();
+            int i = 0;
+            while (i < 10)// "i" ,10 den küçük olduğu sürece döngüye devam eder.
+               {
+                  int sayi = rnd.Next(sayi1, sayi2);// sayi1 ile sayi2 arasında rasgele bir sayı üretip onu "sayi" değişkenine atar.
+                  if (sayilar.Contains(sayi))// "sayilar" dizisinin içinde "sayi" nın değeri olup olmadığını kontrol eder.
+                  continue;// Eğer var ise döngünün içindeki başka hiçbir koda bakmadan devam eder.         
+                  sayilar[i] = sayi;//Burada "sayi"değişkeninin değerini "sayilar"dizisinin "i" ninci elemanına atıyoruz.
+                                    // Yani "i" 5 ise "sayilar" dizisinin 5. elemanına(sayilar[5]) .
+                  i++;// "i" değişkeninin değerini 1 arttırıyoruz.
+                }//Burada ise koşulu kontrol eder ve koşul sağlanıyor ise devam eder ,sağlanmıyor ise döngü biter.
+                //Array.Sort(sayilar);//Bu diziyi küçükten büyüğe sıralar.
+                foreach (int sayi in sayilar)//Burada dizi içindeki sayıları sırasıyla "sayi"değişkenine atar.
+                listBox1.Items.Add(sayi.ToString());//listbox1de görüntülenir.
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();// listeyi temizlemek için.
+            listBox2.Items.Clear();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int sayi = 0;
+            for(int i = 0; i < listBox1.Items.Count; i++)
+            {
+                sayi=int.Parse(listBox1.Items[i].ToString());
+                if( sayi%5==0)
+                {
+                    listBox2.Items.Add(sayi);
+                }
+            }
+        }
+    }
+}
